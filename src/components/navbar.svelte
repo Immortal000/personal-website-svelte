18b0c8881b8fa2d7b6dcace64ec722ab543cf56f
Siddarth Kotthur <avigte@LAPTOP-JNT03HPL.localdomain>
2022-12-29 10:38:08 -0600

    added socials

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,3 +1,13 @@
+<script>
+	import Fa from 'svelte-fa';
+	import { faInbox } from '@fortawesome/free-solid-svg-icons';
+	import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+	const redirect = (url) => {
+		window.open(url);
+	};
+</script>
+
 <div class="container">
 	<div class="links">
 		<ul>
@@ -7,9 +17,24 @@
 	</div>
 	<div class="contacts">
 		<ul>
-			<li>Linkedin</li>
-			<li>Mail</li>
-			<li>Github</li>
+			<li
+				on:click={redirect('https://www.linkedin.com/in/siddarth-kotthur-5a098a1a2/')}
+				on:keydown={redirect('https://www.linkedin.com/in/siddarth-kotthur-5a098a1a2/')}
+			>
+				<Fa icon={faLinkedin} />
+			</li>
+			<li
+				on:click={redirect('mailto:kotthursiddarth@gmail.com')}
+				on:keydown={redirect('mailto:kotthursiddarth@gmail.com')}
+			>
+				<Fa icon={faInbox} />
+			</li>
+			<li
+				on:click={redirect('https://github.com/Immortal000')}
+				on:keydown={redirect('https://github.com/Immortal000')}
+			>
+				<Fa icon={faGithub} />
+			</li>
 		</ul>
 	</div>
 </div>
